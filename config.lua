23e4b304331f31264721f8af748e7dddcfb2b692
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2022-05-29 01:50:41 -0400

    add more locations

--- config.lua
+++ config.lua
@@ -15,8 +15,8 @@ Config.PlateUseSpace = false
 Config.Zones = {
     Shops = {
         {
-            Type = {'sports', 'super'},
-            ShopName = 'Original Car Shop',
+            Type = {'compacts', 'coupes', 'muscle', 'offroad', 'sedans', 'sports', 'sportsclassics', 'super', 'suvs', 'vans'},
+            ShopName = 'Original Vehicle Shop',
             MarkerPosition = {x = -33.7, y = -1102.0, z = 26.4},         
             MarkerSize = {x = 1.5, y = 1.5, z = 1.0},
             MarkerRGB = {r = 255, g = 255, b = 255},
@@ -31,13 +31,13 @@ Config.Zones = {
             DeliveryPosition = {x = -28.6, y = -1085.6, z = 25.5, h = 330.0},
             TestDrive = {
                 Enable = true,
-                Time = 60,
+                Time = 30,
                 Position = {x = -28.6, y = -1085.6, z = 25.5, h = 330.0}
             },
 			Buyable = true
         },
         {
-            Type = {'motorcycles', 'bikes'},
+            Type = {'bikes', 'motorcycles', 'quadbikes'},
             ShopName = 'Original Motor Shop',
             MarkerPosition = {x = -43.4, y = -1104.8, z = 26.4},         
             MarkerSize = {x = 1.5, y = 1.5, z = 1.0},
@@ -53,7 +53,7 @@ Config.Zones = {
             DeliveryPosition = {x = -28.6, y = -1085.6, z = 25.5, h = 330.0},
             TestDrive = {
                 Enable = true,
-                Time = 60,
+                Time = 30,
                 Position = {x = -28.6, y = -1085.6, z = 25.5, h = 330.0}
             },
 			Buyable = true
@@ -75,7 +75,7 @@ Config.Zones = {
             DeliveryPosition = {x = -1174.20, y = -746.28, z = 20.31, h = 221.0},
             TestDrive = {
                 Enable = true,
-                Time = 10,
+                Time = 30,
                 Position = {x = -1142.29, y = -740.75, z = 19.28, h = 286.30}
             },
 			Buyable = false
@@ -125,7 +125,7 @@ Config.Zones = {
 			Buyable = true
         },
         {
-            Type = {'persian_citroen', 'persian_ikco', 'persian_nissan', 'persian_peugeot', 'persian_saipa'},
+            Type = {'astonmartin', 'audi', 'bmw', 'chevrolet', 'dodge', 'ferrari', 'ford', 'honda', 'hyundai', 'jeep', 'kia', 'koenigsegg', 'lamborghini', 'lexus', 'mazda', 'mclaren', 'mercedesbenz', 'mini', 'mitsubishi', 'nissan', 'peugeot', 'porsche', 'subaru', 'tesla', 'toyota'},
             ShopName = 'Real Vehicle Shop',
             MarkerPosition = {x = -798.12, y = -210.17, z = 37.06},         
             MarkerSize = {x = 1.5, y = 1.5, z = 1.0},
@@ -137,12 +137,34 @@ Config.Zones = {
             BlipType = 326,
             BlipColour = 0,
             BlipSize = 1.0,
-            InsideShopPosition = {x = 1225.04, y = 2732.35, z = 37.3, h = 272.12},
-            DeliveryPosition = {x = 1200.51, y = 2726.46, z = 37.50, h = 260.8},
+            InsideShopPosition = {x = -790.36, y = -226.55, z = 36.36, h = 161.57},
+            DeliveryPosition = {x = -773.97, y = -232.39, z = 36.35, h = 201.25},
             TestDrive = {
                 Enable = true,
                 Time = 30,
-                Position = {x = 1200.51, y = 2726.46, z = 37.50, h = 260.8}
+                Position = {x = -759.78, y = -232.47, z = 36.56, h = 206.93}
+            },
+			Buyable = true
+        },
+        {
+            Type = {'drift'},
+            ShopName = 'Drift Vehicle Shop',
+            MarkerPosition = {x = -218.10, y = -289.50, z = 50.12},         
+            MarkerSize = {x = 1.5, y = 1.5, z = 1.0},
+            MarkerRGB = {r = 50, g = 255, b = 255},
+            MarkerDrawDistance = 20.0,
+            MarkerType = 36,
+            EnableSecondaryMarker = false,
+            Blip = true,
+            BlipType = 326,
+            BlipColour = 0,
+            BlipSize = 1.0,
+            InsideShopPosition = {x = -241.36, y = -300.05, z = 49.40, h = 311.81},
+            DeliveryPosition = {x = -209.39, y = -300.75, z = 49.40, h = 340.15},
+            TestDrive = {
+                Enable = true,
+                Time = 60,
+                Position = {x = -209.39, y = -300.75, z = 49.40, h = 340.15}
             },
 			Buyable = true
         },
@@ -174,7 +196,7 @@ Config.Zones = {
 
     SellZones = {
         {
-            Type = {'sports', 'super'},
+            Type = {'compacts', 'coupes', 'muscle', 'offroad', 'sedans', 'sports', 'sportsclassics', 'super', 'suvs', 'vans'},
             MarkerPosition = {x = -36.29, y = -1088.59, z = 26.4},         
             MarkerSize = {x = 1.5, y = 1.5, z = 1.0},
             MarkerRGB = {r = 255, g = 0, b = 0},
@@ -185,10 +207,10 @@ Config.Zones = {
             BlipType = 326,
             BlipColour = 0,
             BlipSize = 1.0,
-            ResellPercentage = 50
+            ResellPercentage = 40
         },
         {
-            Type = {'motorcycles'},
+            Type = {'motorcycles', 'quadbikes'},
             MarkerPosition = {x = -29.28, y = -1092.47, z = 26.4},         
             MarkerSize = {x = 1.5, y = 1.5, z = 1.0},
             MarkerRGB = {r = 255, g = 0, b = 0},
@@ -199,7 +221,7 @@ Config.Zones = {
             BlipType = 326,
             BlipColour = 0,
             BlipSize = 1.0,
-            ResellPercentage = 50
+            ResellPercentage = 40
         },
         {
             Type = {'bikes'},
@@ -213,11 +235,39 @@ Config.Zones = {
             BlipType = 326,
             BlipColour = 0,
             BlipSize = 1.0,
-            ResellPercentage = 50
+            ResellPercentage = 40
         },
         {
-            Type = {'vip_rollsroyce'},
-            MarkerPosition = {x = -1142.26, y = -732.738, z = 20.45},         
+            Type = {'persian_citroen', 'persian_ikco', 'persian_nissan', 'persian_peugeot', 'persian_saipa'},
+            MarkerPosition = {x = 1213.01, y = 2731.91, z = 38.00},         
+            MarkerSize = {x = 1.5, y = 1.5, z = 1.0},
+            MarkerRGB = {r = 255, g = 0, b = 0},
+            MarkerDrawDistance = 20.0,
+            MarkerType = 36,
+            EnableSecondaryMarker = true,
+            Blip = false,
+            BlipType = 326,
+            BlipColour = 0,
+            BlipSize = 1.0,
+            ResellPercentage = 40
+        },
+        {
+            Type = {'astonmartin', 'audi', 'bmw', 'chevrolet', 'dodge', 'ferrari', 'ford', 'honda', 'hyundai', 'jeep', 'kia', 'koenigsegg', 'lamborghini', 'lexus', 'mazda', 'mclaren', 'mercedesbenz', 'mini', 'mitsubishi', 'nissan', 'peugeot', 'porsche', 'subaru', 'tesla', 'toyota'},
+            MarkerPosition = {x = -774.00, y = -226.59, z = 37.1},         
+            MarkerSize = {x = 1.5, y = 1.5, z = 1.0},
+            MarkerRGB = {r = 255, g = 0, b = 0},
+            MarkerDrawDistance = 20.0,
+            MarkerType = 36,
+            EnableSecondaryMarker = true,
+            Blip = false,
+            BlipType = 326,
+            BlipColour = 0,
+            BlipSize = 1.0,
+            ResellPercentage = 40
+        },
+        {
+            Type = {'drift'},
+            MarkerPosition = {x = -199.78, y = -276.43, z = 48.94},         
             MarkerSize = {x = 1.5, y = 1.5, z = 1.0},
             MarkerRGB = {r = 255, g = 0, b = 0},
             MarkerDrawDistance = 20.0,
